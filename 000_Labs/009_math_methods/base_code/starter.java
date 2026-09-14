bdd6a4662234fe66f7c2f087f695c70c999de040
Gtug8584 <GTJedi58@gmail.com>
2026-09-14 21:05:26 +0000

    update

--- 000_Labs/009_math_methods/base_code/starter.java
+++ 000_Labs/009_math_methods/base_code/starter.java
@@ -7,8 +7,13 @@ import java.util.Scanner;
 
 class starter {
 	public static void main(String args[]) {
-		// the string "I love to learn coding remotely." will appear in
-		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+		int r = Math.max(13-6*11,30%7*(-2));
+		double e = Math.sqrt(3*8+31%7);
+		double w = Math.pow(37/3,35%21);
+		double q = Math.max(Math.pow(2,14%3),Math.sqrt(2*6));
+		System.out.println("Maximum = " +r);
+		System.out.println("Square root = "+e);
+        System.out.println("Power = "+ w);
+		System.out.println("Max = "+q);
 	}
 }
